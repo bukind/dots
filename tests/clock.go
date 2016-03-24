68d3170a3fec0b10ea5cb3fbdc6a80d257b3ed24
Dmitry Bukin <bukin.dima@gmail.com>
2016-03-24 22:23:55 +1100

    A simple clock

---
+++ tests/clock.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"github.com/conformal/gotk3/cairo"
+	"github.com/conformal/gotk3/gtk"
+	"math"
+	"time"
+)
+
+func main() {
+	gtk.Init(nil)
+
+	win, _ := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	win.SetTitle("dots")
+	win.Connect("destroy", gtk.MainQuit)
+	win.SetSizeRequest(400, 400)
+	win.SetResizable(false)
+
+	da, _ := gtk.DrawingAreaNew()
+	win.Add(da)
+	win.ShowAll()
+
+	interval := time.Second
+	timer := time.NewTimer(interval)
+	done := make(chan bool)
+	var count float64 = 0
+	deltaPhase := math.Pi * 2 / 60
+
+	go func() {
+		for {
+			select {
+			case <-timer.C:
+				win.QueueDraw()
+				count++
+				timer.Reset(interval)
+			case <-done:
+				break
+			}
+		}
+	}()
+
+	// Event handlers
+	da.Connect("draw", func(da *gtk.DrawingArea, cr *cairo.Context) {
+		cr.SetSourceRGB(0, 0, 0)
+		wx := float64(da.GetAllocatedWidth())
+		wy := float64(da.GetAllocatedHeight())
+		fmt.Println("wx=", wx, ", wy=", wy, ", count=", count)
+		x0 := wx / 2
+		y0 := wy / 2
+		x := x0 * (1.0 + 0.8*math.Sin(count*deltaPhase))
+		y := y0 * (1.0 - 0.8*math.Cos(count*deltaPhase))
+		// cr.Rectangle(x, y, 10.0, 10.0)
+		// cr.Fill()
+		cr.SetLineWidth(10.0)
+		cr.SetSourceRGB(0, 0, 1)
+		cr.MoveTo(x0, y0)
+		cr.LineTo(x, y)
+		cr.Stroke()
+	})
+
+	gtk.Main()
+	close(done)
+}
