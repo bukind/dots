968480e964b31d76cc1d8033c41f566e050a7ab9
Dmitry Bukin <bukin.dima@gmail.com>
2016-03-31 23:40:44 +1100

    Prepare to update kinematics in the right way

--- tests/placeimg.go
+++ tests/placeimg.go
@@ -47,48 +47,28 @@ func mkRand(min, max float64) float64 {
 	return min + rand.Float64()*(max-min)
 }
 
+type State struct {
+	pos    Vector
+	speed  Vector
+	force  Vector
+	energy float64 // potential only
+}
+
 type Sprite struct {
-	surface  *cairo.Surface
-	position Vector
-	speed    Vector
-	force    Vector
-	radius   float64
-	mass     float64
+	surface *cairo.Surface
+	radius  float64
+	mass    float64
+	s       State
 }
 
 func (v *Sprite) Speed() float64 {
-	return VAbs(v.speed)
-}
-
-type SpriteMetric struct {
-	speed    float64
-	momentum float64
-	energy   float64
-	sume     float64
-}
-
-func GetMax(v *SpriteMetric, sprites []*Sprite) {
-	for _, s := range sprites {
-		speed := s.Speed()
-		if speed > v.speed {
-			v.speed = speed
-		}
-		momentum := speed * s.mass
-		if momentum > v.momentum {
-			v.momentum = momentum
-		}
-		energy := momentum * speed / 2
-		if energy > v.energy {
-			v.energy = energy
-		}
-		v.sume += energy
-	}
+	return VAbs(v.s.speed)
 }
 
 func ScaleEnergy(sprites []*Sprite, scale float64) {
 	ratio := math.Sqrt(scale)
-	for _, s := range sprites {
-		s.speed = VScale(s.speed, ratio)
+	for _, v := range sprites {
+		v.s.speed = VScale(v.s.speed, ratio)
 	}
 }
 
@@ -97,6 +77,7 @@ func NewSprite(maxx, maxy, minsize, maxsize int) *Sprite {
 	iwx := rand.Intn(maxsize-minsize+1) + minsize
 	iwy := iwx
 	res.radius = float64(iwx) / 1.2
+	res.mass = res.radius * res.radius
 
 	if res.radius >= float64(maxx/2) || res.radius >= float64(maxy/2) {
 		panic("Too small drawing area")
@@ -115,45 +96,52 @@ func NewSprite(maxx, maxy, minsize, maxsize int) *Sprite {
 	c.SetSourceRGB(red, green, blue)
 	c.Paint()
 
-	res.speed = mkVec(mkRand(-maxspeed, maxspeed), mkRand(-maxspeed, maxspeed))
-	res.position = mkVec(mkRand(res.radius, float64(maxx)-res.radius),
+	res.s.speed = mkVec(mkRand(-maxspeed, maxspeed), mkRand(-maxspeed, maxspeed))
+	res.s.pos = mkVec(mkRand(res.radius, float64(maxx)-res.radius),
 		mkRand(res.radius, float64(maxy)-res.radius))
-	res.mass = res.radius * res.radius
 	return res
 }
 
 func (v *Sprite) UpdatePosition(maxx, maxy, dt float64) {
-	v.speed += VScale(v.force, dt/v.mass)
-	v.position += VScale(v.speed, dt)
+	v.s.pos += VScale(v.s.speed, dt)
 	mx := maxx - v.radius
-	if vx(v.position) < v.radius {
-		v.position = mkVec(2*v.radius-vx(v.position), vy(v.position))
-		if vx(v.speed) < 0 {
-			v.speed = mkVec(-vx(v.speed), vy(v.speed))
+	if vx(v.s.pos) < v.radius {
+		v.s.pos = mkVec(2*v.radius-vx(v.s.pos), vy(v.s.pos))
+		if vx(v.s.speed) < 0 {
+			v.s.speed = mkVec(-vx(v.s.speed), vy(v.s.speed))
 		}
-	} else if vx(v.position) > maxx-v.radius {
-		v.position = mkVec(2*mx-vx(v.position), vy(v.position))
-		if vx(v.speed) > 0 {
-			v.speed = mkVec(-vx(v.speed), vy(v.speed))
+	} else if vx(v.s.pos) > maxx-v.radius {
+		v.s.pos = mkVec(2*mx-vx(v.s.pos), vy(v.s.pos))
+		if vx(v.s.speed) > 0 {
+			v.s.speed = mkVec(-vx(v.s.speed), vy(v.s.speed))
 		}
 	}
 	my := maxy - v.radius
-	if vy(v.position) < v.radius {
-		v.position = mkVec(vx(v.position), 2*v.radius-vy(v.position))
-		if vy(v.speed) < 0 {
-			v.speed = mkVec(vx(v.speed), -vy(v.speed))
+	if vy(v.s.pos) < v.radius {
+		v.s.pos = mkVec(vx(v.s.pos), 2*v.radius-vy(v.s.pos))
+		if vy(v.s.speed) < 0 {
+			v.s.speed = mkVec(vx(v.s.speed), -vy(v.s.speed))
 		}
-	} else if vy(v.position) > maxy-v.radius {
-		v.position = mkVec(vx(v.position), 2*my-vy(v.position))
-		if vy(v.speed) > 0 {
-			v.speed = mkVec(vx(v.speed), -vy(v.speed))
+	} else if vy(v.s.pos) > maxy-v.radius {
+		v.s.pos = mkVec(vx(v.s.pos), 2*my-vy(v.s.pos))
+		if vy(v.s.speed) > 0 {
+			v.s.speed = mkVec(vx(v.s.speed), -vy(v.s.speed))
 		}
 	}
 }
 
-func UpdateForces(sprites []*Sprite, matrix [][]float64) {
-	for _, s := range sprites {
-		s.force = mkVec(0, 0)
+// UpdateDynamics calculates the force and the potential energy of
+// the sprite.
+// The potential energy is by the formula:
+// E = K / r,  r > R0
+// E = K / R0, r < R0
+// where R0 = r1 + r2, r = |Pos1 - Pos2|
+// The force is then:
+// F = K / r^2
+func UpdateDynamics(sprites []*Sprite, matrix [][]float64) {
+	for _, v := range sprites {
+		v.s.force = mkVec(0, 0)
+		v.s.energy = 0
 	}
 
 	for i, v := range sprites {
@@ -168,31 +156,37 @@ func UpdateForces(sprites []*Sprite, matrix [][]float64) {
 			// f = k*(r-1) + k, r < 1
 			//
 			// fx := f * rx / R
-			kf := matrix[i][j] * v.mass * s.mass
-			r0 := v.radius + s.radius
-			rvec := s.position - v.position
-			rbig := VAbs(rvec)
-			r := rbig / r0
-			var f float64
-			if r > 1 {
-				f = kf / (r * r)
-			} else {
-				f = kf * (r - 1)
-				if kf > 0 {
-					f *= 2
-				}
-				f += kf
+			rmin := v.radius + s.radius
+			rvec := s.s.pos - v.s.pos
+			rlen := VAbs(rvec)
+			r := rlen / rmin
+			if r < 1 {
+				r = 1
 			}
-			f /= rbig
+			f := matrix[i][j] * v.mass * s.mass / (r * r) / rlen
 			force := VScale(rvec, f)
-			v.force += force
-			s.force -= force
+			v.s.force += force
+			s.s.force -= force
+			energy := matrix[i][j] * v.mass * s.mass / r
+			v.s.energy += energy
+			s.s.energy += energy
 		}
 	}
 }
 
+func UpdateKinematics(sprites []*Sprite, matrix [][]float64) {
+	// keep the original state
+	// TODO
+
+	// update the force and the energy of the sprites
+	UpdateDynamics(sprites, matrix)
+
+	// update speed
+	// TODO
+}
+
 func (v *Sprite) Paint(cr *cairo.Context) {
-	cr.SetSourceSurface(v.surface, vx(v.position)+v.radius, vy(v.position)+v.radius)
+	cr.SetSourceSurface(v.surface, vx(v.s.pos)+v.radius, vy(v.s.pos)+v.radius)
 	cr.Paint()
 }
 
@@ -270,8 +264,6 @@ func main() {
 	start := now
 	prevstat := now
 
-	var initialEnergySum float64 = 0.
-
 	setup := func(da *gtk.DrawingArea) {
 		sprites = make([]*Sprite, nsprites)
 		matrix = make([][]float64, nsprites)
@@ -281,10 +273,7 @@ func main() {
 		fwy = float64(wy)
 		makeSprites(sprites, wx, wy)
 		makeFeeling(matrix)
-
-		var m SpriteMetric
-		GetMax(&m, sprites)
-		initialEnergySum = m.sume
+		UpdateDynamics(sprites, matrix)
 	}
 
 	// Event handlers
@@ -303,28 +292,24 @@ func main() {
 		now = time.Now()
 		dt := now.Sub(prevtime).Seconds()
 
-		UpdateForces(sprites, matrix)
-
-		var max SpriteMetric
-		GetMax(&max, sprites)
-
-		ScaleEnergy(sprites, initialEnergySum/max.sume)
-
+		// update the location of each sprite
 		for _, s := range sprites {
 			s.UpdatePosition(fwx, fwy, dt)
 		}
+
+		// update the speed of the sprites keeping the balance
+		// of the energy
+		UpdateKinematics(sprites, matrix)
 
 		for _, s := range sprites {
 			s.Paint(cr)
 		}
 
-		if now.Sub(prevstat) > time.Second*5 {
+		if now.Sub(prevstat) > time.Second {
 			prevstat = now
-			fmt.Printf("pass %.0f dt=%.3f perf=%v maxv/p/e=%.1f/%.1e/%.1e sume=%.1e\n",
+			fmt.Printf("pass %.0f dt=%.3f perf=%v\n",
 				now.Sub(start).Seconds(),
-				dt, time.Now().Sub(now),
-				max.speed, max.momentum, max.energy,
-				max.sume)
+				dt, time.Now().Sub(now))
 		}
 
 		// restart the drawing
