beb02f572393c2d6837e04b6472f549a1236843a
Dmitry Bukin <bukin.dima@gmail.com>
2016-05-27 14:08:37 +1000

    Print percentage, fix

--- convay01.go
+++ convay01.go
@@ -396,9 +396,8 @@ func areaDraw(da *gtk.DrawingArea, cr *cairo.Context, pg *Playground) {
 	cr.SetFontSize(12.)
 	total := float64(pg.cellsPerRow * len(pg.area))
 	cr.ShowText(fmt.Sprintf("steps:%d cells:%d/%.1f%%  old:%d/%.1f%%",
-		pg.iterations, olds+news, olds,
-		float64(olds+news)*100/total,
-		float64(olds)*100/total))
+		pg.iterations, olds+news, float64(olds+news)*100/total,
+		olds, float64(olds)*100/total))
 	cr.Stroke()
 	if pg.repeats != 0 {
 		pg.StepAndDraw()
